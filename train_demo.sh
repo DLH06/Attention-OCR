2d22a567fe0c53a14ff4fcd77d79f98da4a6bd7b
DLH06 <daolehuy99@gmail.com>
2019-12-03 14:55:38 +0700

    eww

--- train_demo.sh
+++ train_demo.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-python src/launcher.py \
+python3 src/launcher.py \
 	--phase=train \
-	--data-path=/media/data2/sivankeret/Datasets/mnt/ramdisk/max/90kDICT32px/annotation_train_words.txt \
-	--data-base-dir=/media/data2/sivankeret/Datasets/mnt/ramdisk/max/90kDICT32px \
-	--log-path=log_01_16.txt \
+	--data-path=/home/dlh06/TextRecognitionDataGenerator/TextRecognitionDataGenerator/images/labels.txt \
+	--data-base-dir=/home/dlh06/TextRecognitionDataGenerator/TextRecognitionDataGenerator/images \
+	--log-path=log.txt \
 	--attn-num-hidden 256 \
 	--batch-size 64 \
-	--model-dir=model_01_16 \
-	--initial-learning-rate=1.0 \
+	--initial-learning-rate=0.001 \
 	--no-load-model \
-	--num-epoch=3 \
-	--gpu-id=0 \
-	--use-gru \
+	--num-epoch=100 \
 	--steps-per-checkpoint=2000 \
     --target-embedding-size=10
